24769c73839518db8c6a9b7d2969fce9a3dd50a5
CharaeKeow <34004186+CharaeKeow@users.noreply.github.com>
2019-10-22 10:38:16 +0800

    Create swapNumv2.cpp

---
+++ swapNumv2.cpp
@@ -0,0 +1,32 @@
+// A simple program to swap two numbers passed into a function using pass by reference.
+// Now I see that with pass by ref, we can retain the value altered by the function.
+// Like for this case, the value of the swapped pair are retained after the function call.
+
+#include "stdafx.h"
+#include <iostream>
+#include <string>
+using namespace std;
+
+void swap(float &, float &);
+
+int main()
+{
+	float a, b;
+	cout << "Enter two values: " << endl;
+	cin >> a >> b;
+	cout << "The original pair is: " << endl;
+	cout << "a = " << a << ", b = " << b << endl;
+	swap(a, b);
+	cout << "The original pair is: " << endl;
+	cout << "a = " << a << ", b = " << b << endl;
+	return 0;
+}
+
+void swap(float &a, float &b) {
+	float temp = a; //temp to prevent overriding
+	a = b;
+	b = temp; 
+	cout << "The value after being swapped is: " << endl;
+	cout << "a = " << a;
+	cout << "\nb = " << b << endl;
+}
